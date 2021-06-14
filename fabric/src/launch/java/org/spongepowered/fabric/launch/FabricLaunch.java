98df84d5dc4312390bbe969103377e8b9c53737f
LogicFan <38597904+LogicFan@users.noreply.github.com>
2021-06-14 18:25:56 -0400

    comment out codes for now

--- fabric/src/launch/java/org/spongepowered/fabric/launch/FabricLaunch.java
+++ fabric/src/launch/java/org/spongepowered/fabric/launch/FabricLaunch.java
@@ -47,7 +47,7 @@ import java.util.Enumeration;
 public abstract class FabricLaunch extends Launch {
 
     private final Stage injectionStage;
-    private PluginContainer vanillaPlugin;
+    private PluginContainer fabricPlugin;
 
     protected FabricLaunch(final FabricPluginEngine pluginEngine, final Stage injectionStage) {
         super(pluginEngine, new FabricPluginManager());
@@ -56,7 +56,7 @@ public abstract class FabricLaunch extends Launch {
 
     @Override
     public final boolean isVanilla() {
-        return true;
+        return false;
     }
 
     @Override
@@ -71,68 +71,68 @@ public abstract class FabricLaunch extends Launch {
 
     @Override
     public final PluginContainer getPlatformPlugin() {
-        if (this.vanillaPlugin == null) {
-            this.vanillaPlugin = this.getPluginManager().plugin("spongevanilla").orElse(null);
+        if (this.fabricPlugin == null) {
+            this.fabricPlugin = this.getPluginManager().plugin("spongevanilla").orElse(null);
 
-            if (this.vanillaPlugin == null) {
+            if (this.fabricPlugin == null) {
                 throw new RuntimeException("Could not find the plugin representing SpongeVanilla, this is a serious issue!");
             }
         }
 
-        return this.vanillaPlugin;
+        return this.fabricPlugin;
     }
 
     @Override
     protected final void createPlatformPlugins(final PluginEngine pluginEngine) {
-        final Path gameDirectory = this.pluginEngine.getPluginEnvironment().blackboard().get(PluginKeys.BASE_DIRECTORY)
-                .orElseThrow(() -> new RuntimeException("The game directory has not been added to the environment!"));
-
-        try {
-            // This is a bit nasty, but allows Sponge to detect builtin platform plugins when it's not the first entry on the classpath.
-            final URL classUrl = FabricLaunch.class.getResource("/" + FabricLaunch.class.getName().replace('.', '/') + ".class");
-
-            Collection<PluginMetadata> read = null;
-
-            // In production, let's try to ensure we can find our descriptor even if we're not first on the classpath
-            if (classUrl.getProtocol().equals("jar")) {
-                // Extract the path of the underlying jar file, and parse it as a path to normalize it
-                final String[] classUrlSplit = classUrl.getPath().split("!");
-                final Path expectedFile = Paths.get(new URL(classUrlSplit[0]).toURI());
-
-                // Then go through every possible resource
-                final Enumeration<URL> manifests =
-                        FabricLaunch.class.getClassLoader().getResources("/META-INF/" + JVMPluginResourceLocatorService.DEFAULT_METADATA_FILENAME);
-                while (manifests.hasMoreElements()) {
-                    final URL next = manifests.nextElement();
-                    if (!next.getProtocol().equals("jar")) {
-                        continue;
-                    }
-
-                    // And stop when the normalized jar in that resource matches the URL of the jar that loaded VanillaLaunch?
-                    final String[] pathSplit = next.getPath().split("!");
-                    if (pathSplit.length == 2) {
-                        if (Paths.get(new URL(pathSplit[0]).toURI()).equals(expectedFile)) {
-                            read = PluginMetadataHelper.builder().build().read(next.openStream());
-                            break;
-                        }
-                    }
-                }
-            }
-
-            if (read == null) { // other measures failed, fall back to directly querying the classpath
-                read = PluginMetadataHelper.builder().build().read(FabricLaunch.class.getResourceAsStream(
-                        "/META-INF/" + JVMPluginResourceLocatorService.DEFAULT_METADATA_FILENAME));
-            }
-            if (read == null) {
-                throw new RuntimeException("Could not determine location for implementation metadata!");
-            }
-
-            for (final PluginMetadata metadata : read) {
-                this.getPluginManager().addDummyPlugin(new DummyPluginContainer(metadata, gameDirectory, this.getLogger(), this));
-            }
-        } catch (final IOException | URISyntaxException e) {
-            throw new RuntimeException("Could not load metadata information for the implementation! This should be impossible!");
-        }
+//        final Path gameDirectory = this.pluginEngine.getPluginEnvironment().blackboard().get(PluginKeys.BASE_DIRECTORY)
+//                .orElseThrow(() -> new RuntimeException("The game directory has not been added to the environment!"));
+//
+//        try {
+//            // This is a bit nasty, but allows Sponge to detect builtin platform plugins when it's not the first entry on the classpath.
+//            final URL classUrl = FabricLaunch.class.getResource("/" + FabricLaunch.class.getName().replace('.', '/') + ".class");
+//
+//            Collection<PluginMetadata> read = null;
+//
+//            // In production, let's try to ensure we can find our descriptor even if we're not first on the classpath
+//            if (classUrl.getProtocol().equals("jar")) {
+//                // Extract the path of the underlying jar file, and parse it as a path to normalize it
+//                final String[] classUrlSplit = classUrl.getPath().split("!");
+//                final Path expectedFile = Paths.get(new URL(classUrlSplit[0]).toURI());
+//
+//                // Then go through every possible resource
+//                final Enumeration<URL> manifests =
+//                        FabricLaunch.class.getClassLoader().getResources("/META-INF/" + JVMPluginResourceLocatorService.DEFAULT_METADATA_FILENAME);
+//                while (manifests.hasMoreElements()) {
+//                    final URL next = manifests.nextElement();
+//                    if (!next.getProtocol().equals("jar")) {
+//                        continue;
+//                    }
+//
+//                    // And stop when the normalized jar in that resource matches the URL of the jar that loaded VanillaLaunch?
+//                    final String[] pathSplit = next.getPath().split("!");
+//                    if (pathSplit.length == 2) {
+//                        if (Paths.get(new URL(pathSplit[0]).toURI()).equals(expectedFile)) {
+//                            read = PluginMetadataHelper.builder().build().read(next.openStream());
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (read == null) { // other measures failed, fall back to directly querying the classpath
+//                read = PluginMetadataHelper.builder().build().read(FabricLaunch.class.getResourceAsStream(
+//                        "/META-INF/" + JVMPluginResourceLocatorService.DEFAULT_METADATA_FILENAME));
+//            }
+//            if (read == null) {
+//                throw new RuntimeException("Could not determine location for implementation metadata!");
+//            }
+//
+//            for (final PluginMetadata metadata : read) {
+//                this.getPluginManager().addDummyPlugin(new DummyPluginContainer(metadata, gameDirectory, this.getLogger(), this));
+//            }
+//        } catch (final IOException | URISyntaxException e) {
+//            throw new RuntimeException("Could not load metadata information for the implementation! This should be impossible!");
+//        }
     }
 
     @Override
